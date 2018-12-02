cdd6acb243b38a5b39087400ee201986267e05a5
Faey Hoffmeyer <hoffmeyer25@gmail.com>
2018-12-02 00:57:59 -0500

    Move file into it's own file in case I need it for more days

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -15,16 +15,3 @@ fn main() -> std::result::Result<(), Error> {
     println!("{:#?}", frequency);
     Ok(())
 }
-
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
-
-fn file(filename: &str) -> std::result::Result<std::string::String, Error> {
-    let file = File::open(filename)?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents)?;
-    Ok(contents)
-}
-
--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,15 @@ impl From<std::num::ParseIntError> for Error {
 impl From<std::option::NoneError> for Error {
     fn from(e: std::option::NoneError) -> Error { Error::NoneE(e) }
 }
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+pub fn file(filename: &str) -> std::result::Result<std::string::String, Error> {
+    let file = File::open(filename)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    Ok(contents)
+}
